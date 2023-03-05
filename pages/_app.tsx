fcd247521e69a4daa7bc2349930dac1aedd826bf
Štěpán Vácha <step.vacha@gmail.com>
2023-03-05 21:32:26 +0100

    🚀 dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
-    <>
+    <ThemeProvider>
       {router.pathname != "/" && (
         <header className="flex flex-row p-3 justify-center">
           <div className="order-1 text-center">
@@ -19,6 +20,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <main>
         <Component {...pageProps} />
       </main>
-    </>
+    </ThemeProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,41 @@
 import Link from "next/link";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 export default function Index() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return <></>;
+
+  const toggleDarkMode = (dark: boolean) => {
+    if (dark) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+    // setMounted(checked);
+  };
+
   return (
     <div className="flex justify-center min-h-screen flex-col">
-      <div className="order-1 text-center items-end h-1/2 p-2">
-        <p className="font-bold text-6xl">hi, I&apos;m Štěpán</p>
+      <div className="order-1 justify-center items-end h-1/2 p-2 flex flex-row">
+        <div className="order-1 text-right">
+          <p className="font-bold text-6xl">hi, I&apos;m Štěpán</p>
+        </div>
+        <div className="order-2">
+          <DarkModeSwitch
+            style={{ marginBottom: "2rem" }}
+            checked={theme === "dark"}
+            onChange={toggleDarkMode}
+            size={12}
+          />
+        </div>
       </div>
       <div className="order-2 items-start h-1/2 flex flex-row justify-center">
         <div className="order-1 text-center p-2">
