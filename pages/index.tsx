50ba55a12256eccae45ea8e45d714e9ffbd65fe2
Štěpán Vácha <step.vacha@gmail.com>
2023-03-05 22:54:03 +0100

    better dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
-import { DarkModeSwitch } from "react-toggle-dark-mode";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
 
 export default function Index() {
   const { theme, setTheme } = useTheme();
@@ -13,52 +13,50 @@ export default function Index() {
 
   if (!mounted) return <></>;
 
-  const toggleDarkMode = (dark: boolean) => {
-    if (dark) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-    // setMounted(checked);
-  };
-
   return (
-    <div className="flex justify-center min-h-screen flex-col">
-      <div className="order-1 justify-center items-end h-1/2 p-2 flex flex-row">
-        <div className="order-1 text-right">
-          <p className="font-bold text-6xl">hi, I&apos;m Štěpán</p>
+    <>
+      <div className="flex justify-center h-screen flex-col">
+        <header className="flex flex-row p-3 justify-center">
+          <button
+            onClick={() => {
+              setTheme(theme === "dark" ? "light" : "dark");
+            }}
+          >
+            {theme === "dark" ? (
+              <SunIcon className="w-6" />
+            ) : (
+              <MoonIcon className="w-6" />
+            )}
+          </button>
+        </header>
+        <div className="order-1 justify-center items-end h-1/2 p-2 flex flex-1 flex-row">
+          <div className="order-1 text-right">
+            <p className="font-bold text-6xl">hi, I&apos;m Štěpán</p>
+          </div>
         </div>
-        <div className="order-2">
-          <DarkModeSwitch
-            style={{ marginBottom: "2rem" }}
-            checked={theme === "dark"}
-            onChange={toggleDarkMode}
-            size={12}
-          />
+        <div className="order-2 items-start h-1/2 flex flex-row justify-center">
+          <div className="order-1 text-center p-2">
+            <p className="text-xl hover:text-2xl hover:font-bold transition-opacity">
+              <Link href="/about">About me</Link>
+            </p>
+          </div>
+          <div className="order-2 text-center p-2">
+            <p className="text-xl hover:text-2xl hover:font-bold transition-opacity">
+              <Link href="/">Blog</Link>
+            </p>
+          </div>
+          <div className="order-2 text-center p-2">
+            <p className="text-xl hover:text-2xl hover:font-bold transition-opacity">
+              <Link href="/">Projects</Link>
+            </p>
+          </div>
+          <div className="order-3 text-center p-2">
+            <p className="text-xl hover:text-2xl hover:font-bold transition-opacity">
+              <Link href="/">Coffee</Link>
+            </p>
+          </div>
         </div>
       </div>
-      <div className="order-2 items-start h-1/2 flex flex-row justify-center">
-        <div className="order-1 text-center p-2">
-          <p className="text-xl hover:text-2xl hover:font-bold transition-opacity">
-            <Link href="/about">About me</Link>
-          </p>
-        </div>
-        <div className="order-2 text-center p-2">
-          <p className="text-xl hover:text-2xl hover:font-bold transition-opacity">
-            <Link href="/">Blog</Link>
-          </p>
-        </div>
-        <div className="order-2 text-center p-2">
-          <p className="text-xl hover:text-2xl hover:font-bold transition-opacity">
-            <Link href="/">Projects</Link>
-          </p>
-        </div>
-        <div className="order-3 text-center p-2">
-          <p className="text-xl hover:text-2xl hover:font-bold transition-opacity">
-            <Link href="/">Coffee</Link>
-          </p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
