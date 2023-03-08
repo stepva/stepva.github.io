45ee5bc8c9d2eeee2a5a0bc2f964954ea663d41a
Štěpán Vácha <step.vacha@gmail.com>
2023-03-08 23:06:24 +0100

    new dark mode!
    Co-authored-by: Jordan Fisher <jdjfisher@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,32 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState<"dark" | "light">("dark");
+  const router = useRouter();
+
   return (
-    <main className="p-3">
-      <Component {...pageProps} />
+    <main className={theme}>
+      <div className="dark:text-white dark:bg-black">
+        {router.pathname === "/" && (
+          <header className="flex flex-row p-3 justify-center">
+            <button
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              {theme == "dark" ? (
+                <SunIcon className="w-6 hover:animate-spin" />
+              ) : (
+                <MoonIcon className="w-6 hover:animate-spin" />
+              )}
+            </button>
+          </header>
+        )}
+
+        <Component {...pageProps} />
+      </div>
     </main>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,35 @@
 import Link from "next/link";
-import { useTheme } from "next-themes";
-import { useState, useEffect } from "react";
-import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
 
 export default function Index() {
   return (
-    <>
-      <div className="flex justify-center h-screen flex-col">
-        <header className="flex flex-row p-3 justify-center">
-          <button>
-            <SunIcon className="w-6 hover:animate-spin" />
-          </button>
-        </header>
-        <div className="justify-center items-end h-1/2 p-2 flex flex-1 flex-row">
-          <div className="text-center">
-            <p className="font-bold text-6xl">hi, I&apos;m Štěpán</p>
-          </div>
-        </div>
-        <div className="items-start h-1/2 flex flex-row justify-center">
-          <div className="text-center p-2">
-            <p className="text-xl hover:font-bold">
-              <Link href="/about">About me</Link>
-            </p>
-          </div>
-          <div className="text-center p-2">
-            <p className="text-xl hover:font-bold">
-              <Link href="/">Blog</Link>
-            </p>
-          </div>
-          <div className="text-center p-2">
-            <p className="text-xl hover:font-bold">
-              <Link href="/">Projects</Link>
-            </p>
-          </div>
-          <div className="text-center p-2">
-            <p className="text-xl hover:font-bold">
-              <Link href="/">Coffee</Link>
-            </p>
-          </div>
+    <div className="flex justify-center flex-col h-screen">
+      <div className="justify-center items-end h-1/2 p-2 flex flex-1 flex-row">
+        <div className="text-center">
+          <p className="font-bold text-6xl">hi, I&apos;m Štěpán</p>
         </div>
       </div>
-    </>
+      <div className="items-start h-1/2 flex flex-row justify-center">
+        <div className="text-center p-2">
+          <p className="text-xl hover:font-bold">
+            <Link href="/about">About me</Link>
+          </p>
+        </div>
+        <div className="text-center p-2">
+          <p className="text-xl hover:font-bold">
+            <Link href="/">Blog</Link>
+          </p>
+        </div>
+        <div className="text-center p-2">
+          <p className="text-xl hover:font-bold">
+            <Link href="/">Projects</Link>
+          </p>
+        </div>
+        <div className="text-center p-2">
+          <p className="text-xl hover:font-bold">
+            <Link href="/">Coffee</Link>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
