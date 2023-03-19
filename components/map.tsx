eb8916a66b7a5d49d2338f9de7413041d96d1486
Štěpán Vácha <step.vacha@gmail.com>
2023-03-19 22:52:07 +0100

    map marker cluster

--- components/map.tsx
+++ components/map.tsx
@@ -3,6 +3,9 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
 import Link from "next/link";
+// @ts-ignore
+import MarkerClusterGroup from "@changey/react-leaflet-markercluster";
+require("@changey/react-leaflet-markercluster/dist/styles.min.css"); // inside .js file
 
 const Map = () => {
   return (
@@ -16,49 +19,51 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[48.1569, 17.12192]}>
-        <Popup>
-          🇸🇰 Good Times Coffee Roasters <br />
-          <Link href="/coffee/good-times-coffee-finca-potosi">
-            <div className="text-center mt-1">🇨🇴 Finca Potosí</div>
-          </Link>
-        </Popup>
-      </Marker>
-      <Marker position={[52.54, 13.40538]}>
-        <Popup>
-          🇩🇪 Bonanza Coffee Roasters <br />
-          <Link href="/coffee/bonanza-sasaba">
-            <div className="text-center mt-1">🇪🇹 Sasaba</div>
-          </Link>
-        </Popup>
-      </Marker>
-      <Marker position={[48.85452, 16.04826]}>
-        <Popup>
-          🇨🇿 BALANCE coffee & wine <br />
-          <Link href="/coffee/balance-agaro-jimma">
-            <div className="text-center mt-1">🇪🇹 Agaro & Jimma</div>
-          </Link>
-        </Popup>
-      </Marker>
-      <Marker position={[54.6823, 25.26538]}>
-        <Popup>
-          🇱🇹 Taste Map Coffee Roasters <br />
-          <Link href="/coffee/masira-taste-map">
-            <div className="text-center mt-1">🇺🇬 Masira</div>
-          </Link>
-        </Popup>
-      </Marker>
-      <Marker position={[48.21486, 16.36189]}>
-        <Popup>
-          🇦🇹 Jonas Reindl Coffee Roasters <br />
-          <Link href="/coffee/dumtu-village-jonas-reindl">
-            <div className="text-center mt-1">🇪🇹 Dimtu Village</div>
-          </Link>
-        </Popup>
-      </Marker>
-      {/* <Marker position={[48.21729, 16.35114]}>
+      <MarkerClusterGroup showCoverageOnHover={false}>
+        <Marker position={[48.1569, 17.12192]}>
+          <Popup>
+            🇸🇰 Good Times Coffee Roasters <br />
+            <Link href="/coffee/good-times-coffee-finca-potosi">
+              <div className="text-center mt-1">🇨🇴 Finca Potosí</div>
+            </Link>
+          </Popup>
+        </Marker>
+        <Marker position={[52.54, 13.40538]}>
+          <Popup>
+            🇩🇪 Bonanza Coffee Roasters <br />
+            <Link href="/coffee/bonanza-sasaba">
+              <div className="text-center mt-1">🇪🇹 Sasaba</div>
+            </Link>
+          </Popup>
+        </Marker>
+        <Marker position={[48.85452, 16.04826]}>
+          <Popup>
+            🇨🇿 BALANCE coffee & wine <br />
+            <Link href="/coffee/balance-agaro-jimma">
+              <div className="text-center mt-1">🇪🇹 Agaro & Jimma</div>
+            </Link>
+          </Popup>
+        </Marker>
+        <Marker position={[54.6823, 25.26538]}>
+          <Popup>
+            🇱🇹 Taste Map Coffee Roasters <br />
+            <Link href="/coffee/masira-taste-map">
+              <div className="text-center mt-1">🇺🇬 Masira</div>
+            </Link>
+          </Popup>
+        </Marker>
+        <Marker position={[48.21486, 16.36189]}>
+          <Popup>
+            🇦🇹 Jonas Reindl Coffee Roasters <br />
+            <Link href="/coffee/dumtu-village-jonas-reindl">
+              <div className="text-center mt-1">🇪🇹 Dimtu Village</div>
+            </Link>
+          </Popup>
+        </Marker>
+        {/* <Marker position={[48.21729, 16.35114]}>
         <Popup>CoffeePirates</Popup>
       </Marker> */}
+      </MarkerClusterGroup>
     </MapContainer>
   );
 };
